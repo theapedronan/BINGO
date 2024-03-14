e06172fe392a7c14fb23e1d70d9fab320116a30c
theapedronan <theaangelacaramat08@gmail.com>
2024-03-14 14:50:24 +0800

    fourth commit

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ function generateNumber(max) {
     // GT >, GTE > =, LT <, LTE < =, EQ =, NEQ ! +, AND &&, OR ||
     if (randomNumber <= 15) {
         console.log('The number is in B. ');
-    } else if (randomNumber <= 16 && randomNumber <= 30) {
+    } else if (randomNumber >= 16 && randomNumber <= 30) {
         console.log('The number is in I.');
     } else
-        if (randomNumber >= 31 && randomNumber < + 45) {
+        if (randomNumber >= 31 && randomNumber <= + 45) {
             console.log('The number is in N');
         } else
-            if (randomNumber <= 46 && randomNumber < + 60) {
+            if (randomNumber >= 46 && randomNumber <= + 60) {
                 console.log('The number is in G');
             } else
-                if (randomNumber <= 61 && randomNumber < + 75) {
+                if (randomNumber >= 61 && randomNumber <= + 75) {
                     console.log('The number is in O');
                 } else {
                     console.log('The number is in valid');
